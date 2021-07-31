da61168c041ec5b297e0e627ff331369c5ee60c8
stephanxiayu <Stephan_Granzow@web.de>
2021-07-31 09:05:31 +0100

    Changes to be committed: 	modified:   lib/main.dart 	deleted:    lib/startseite.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'package:praktikant/startseite.dart';
+import 'package:praktikant/home.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,8 +17,71 @@ class MyApp extends StatelessWidget {
 
      
       ),
-      home: VideoTest(),
+      home: Homepage(),
     );
   }
 }
 
+
+class Homepage extends StatefulWidget {
+  const Homepage({ Key key }) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+    int _currentIndex = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("Stephans Praktikant"))),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          children: <Widget>[
+            Container(color: Colors.blueGrey,),
+            Container(color: Colors.teal,),
+       
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            title: Text('Item One'),
+            icon: Icon(Icons.home)
+          ),
+          BottomNavyBarItem(
+            title: Text('Item Two'),
+            icon: Icon(Icons.apps)
+          ),
+        
+        ],
+      ),
+    );
+  }
+}
